757fb8c539f4424c16b39144c8dbeda8876a8043
vbhardw4 <vbhardw4@uwo.ca>
2023-02-01 09:05:07 -0500

    Added Navbar component to route the requests to various components in single page application

---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Courses from './Courses';
+import Results from './Results';
+import Students from './Students';
+import Home from './Home';
+import '../index.css';
+
+const Navbar = () => {
+  return (
+    <Router>
+      <nav >
+        <ul className="menu-bar">
+          <li className="menu-item"><Link to="/">Home</Link></li>
+          <li className="menu-item"><Link to="/students">Students</Link></li>
+          <li className="menu-item"><Link to="/courses">Courses</Link></li>
+          <li className="menu-item"><Link to="/results">Results</Link></li>
+          
+        </ul>
+      </nav>
+      <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/students" element={<Students/>}/>
+          <Route exact path="/courses" element={<Courses/>}/>
+          <Route path="/results" element={<Results/>}/>
+        </Routes>
+    </Router>
+  );
+};
+
+export default Navbar;
