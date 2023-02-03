c82371938ee0ef135ace67b8fbd7b199d3817f28
vbhardw4 <vbhardw4@uwo.ca>
2023-02-02 19:54:33 -0500

    Implemented separate reducers for separation of concerns and incorporated add student and delete student actions

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,19 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import studentReducer from '../reducers/studentReducer';
 
-const initialState = {
-  students: []
-};
-
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'ADD_STUDENT':
-      return {
-        ...state,
-        students: [...state.students, action.student]
-      };
-    default:
-      return state;
+export const store = configureStore({
+  reducer:{
+    students: studentReducer
   }
-};
-
-export const store = configureStore({reducer:reducer});
+});
